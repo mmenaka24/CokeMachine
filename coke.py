53b67abd741734575dd6c78989d3a2d626794f52
mmenaka24 <menaka_maruthainar@live.com>
2025-02-03 18:46:24 +0000

    tidy
    moves validCoins variable declaration out of while loop

--- coke.py
+++ coke.py
@@ -1,12 +1,11 @@
 def main():
     amountDue = 50
+    validCoins = [25, 10, 5]
 
     while amountDue > 0:
         print("Amount Due: " + str(amountDue))
 
         coin = int(input("Insert Coin: "))
-
-        validCoins = [25, 10, 5]
 
         if coin in validCoins:
             amountDue -= coin
